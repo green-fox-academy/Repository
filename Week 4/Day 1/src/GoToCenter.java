b0f99424548e4703b460181bcab18cfefa53e39c
Markovics Roland <rolandmarkovics93@gmail.com>
2020-06-10 19:36:50 +0200

    GoToCenter Added

--- Week 4/Day 1/src/GoToCenter.java
+++ Week 4/Day 1/src/GoToCenter.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-
+import java.util.Random;
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
@@ -10,7 +10,23 @@ public class GoToCenter {
     // The x and y coordinates of the line's starting point and the graphics
     // and draws a line from that point to the center of the canvas.
     // Draw at least 3 lines with that function using a loop.
-    
+
+    for (int i = 0; i < 5; i++) {
+      drawLine(graphics, randomStart(), randomStart());
+    }
+  }
+
+  public static int randomStart() {
+    Random random = new Random();
+    int max = (321);
+    return random.nextInt(max);
+  }
+
+  public static void drawLine (Graphics graphics, int x, int y) {
+    Random random = new Random();
+    int horizontalMiddle = (WIDTH / 2);
+    int verticalMiddle = (HEIGHT / 2);
+    graphics.drawLine(x, y, horizontalMiddle, verticalMiddle);
   }
 
   // Don't touch the code below
