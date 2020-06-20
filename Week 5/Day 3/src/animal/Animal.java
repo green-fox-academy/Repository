21807cdca0bdfa6b23aea65c94013a2f46a9ccc5
Markovics Roland <rolandmarkovics93@gmail.com>
2020-06-20 14:01:12 +0200

    Animal Added

---
+++ Week 5/Day 3/src/animal/Animal.java
@@ -0,0 +1,26 @@
+package animal;
+
+public class Animal {
+  private int hunger = 50;
+  private int thirst = 50;
+
+  public Animal() {
+  }
+
+  public void eat() {
+    hunger -= 1;
+  }
+
+  public void drink() {
+    thirst -= 1;
+  }
+
+  public void play() {
+    hunger += 1;
+    thirst += 1;
+  }
+
+  public void status(){
+    System.out.println(hunger + "\n" + thirst + "\n");
+  }
+}
