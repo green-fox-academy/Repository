151002dbb4e4d5a4c3b1f134c399243536408e5c
Markovics Roland <rolandmarkovics93@gmail.com>
2020-07-18 14:57:58 +0200

    Task Adder Added

--- Week 8/Day 2/src/ToDo.java
+++ Week 8/Day 2/src/ToDo.java
@@ -1,22 +1,30 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ToDo {
 
   public static void main(String[] args) {
-    List<String> toDoList = new ArrayList<>();
+    Path path = Paths.get("ToDo.txt");
+    //region List Creation
+    List<String> toDoList = Collections.emptyList();
+    try {
+      toDoList = Files.readAllLines(Paths.get("Todo.txt"), StandardCharsets.UTF_8);
+    } catch (Exception ex) {
+      System.out.println("Cannot read file");
+    }
+    //endregion
     if (args.length == 0) {
       linkCommandLine();
     } else if (args[0].equals("-l")) {
-      listTask();
+      listTasks();
+    } else if (args[0].equals("-a")) {
+      addTask(path, toDoList, args[1]);
     }
-
-    addTask(toDoList, "Walk the Dog");
 
   }
 
@@ -28,28 +36,29 @@ public class ToDo {
             "Command line arguments:\n" +
             "    -l   Lists all the tasks\n" +
             "    -a   Adds a new task\n" +
-            "    -r   Removes an task\n" +
+            "    -r   Removes a task\n" +
             "    -c   Completes an task\n");
   }
 
-  public static void addTask(List toDoList, String task) {
-    toDoList.add(task);
+  public static void addTask(Path path, List toDoList, String task) {
     try {
-      Path path = Paths.get("ToDo.txt");
+      toDoList.add(task);
       Files.write(path, toDoList);
     } catch (Exception ex) {
       System.out.println("Unable to write file");
     }
   }
 
-  public static void listTask() {
+  public static void listTasks() {
+    int counter = 0;
     try {
       Files.readAllLines(Paths.get("ToDo.txt"));
       if (Files.size(Paths.get("ToDo.txt")) == 0) {
         System.out.println("No todos for today! :)");
       } else {
         for (String task : Files.readAllLines(Paths.get("ToDo.txt"))) {
-          System.out.println(task);
+          counter += 1;
+          System.out.println(counter + " - " + task);
         }
       }
     } catch (Exception ex) {
